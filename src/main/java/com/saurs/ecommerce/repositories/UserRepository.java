3a3edd47cb39523ddeafcae46da928a860c64dee
samluiz <samuelluizrl@outlook.com>
2023-01-25 13:21:28 -0300

    component registration and get operations done

--- src/main/java/com/saurs/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/saurs/ecommerce/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.saurs.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.saurs.ecommerce.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   
 }
